c718351aa2b69af8cc6834311ae0373cf55705e9
shaw342 <shawanbarua70@gmail.com>
2024-11-01 21:41:29 +0100

    implement calandar

--- backend /authMiddleware/auth.go
+++ backend /authMiddleware/auth.go
@@ -1,1 +1,17 @@
 package authMiddleware
+
+import "github.com/gin-gonic/gin"
+
+func authMiddleware() gin.HandlerFunc{
+	return func(ctx *gin.Context)  {
+		tokenString := ctx.GetHeader("Authorization")
+
+		if tokenString == ""{
+			ctx.JSON(401, gin.H{"error": "Unauthorized"})
+            ctx.Abort()
+            return
+		}
+
+		
+	}
+}

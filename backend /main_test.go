e8ae5d575ea113c5c9e536e97c5cdd0de40e91ae
shaw342 <shawanbarua70@gmail.com>
2024-12-07 15:04:58 +0100

    add test for task an project

--- backend /main_test.go
+++ backend /main_test.go
@@ -4,9 +4,11 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/gin-gonic/gin"
@@ -16,102 +18,131 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func SetUpRouter() *gin.Engine{
+func SetUpRouter() *gin.Engine {
 	router := gin.Default()
 	return router
 }
 
 func TestCreateUser(t *testing.T) {
 
-    err := godotenv.Load()
-    if err != nil {
-        fmt.Println("Error loading .env file")
-    }
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
 
-    value := os.Getenv("FAUNA_SECRET")
-    fmt.Print(value)
-    os.Setenv("FAUNA_SECRET",value)
-    router := gin.New()
+	value := os.Getenv("FAUNA_SECRET")
+	fmt.Print(value)
+	os.Setenv("FAUNA_SECRET", value)
+	router := gin.New()
 
-    router.POST("/api/v1/user", repository.CreateUser)
+	router.POST("/api/v1/user", repository.CreateUser)
 
-    user := model.User{
-        FirstName: "shawan",
-        LastName: "barua",
-        Email:    "shawan@example.com",
-        Password: "password",
-    }
+	user := model.User{
+		FirstName: "shawan",
+		LastName:  "barua",
+		Email:     "shawan@example.com",
+		Password:  "password",
+	}
 
-    jsonData, err := json.Marshal(user)
-    if err != nil {
-        t.Fatal(err)
-    }
+	jsonData, err := json.Marshal(user)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-    req, err := http.NewRequest("POST", "/api/v1/user", bytes.NewBuffer(jsonData))
-    if err != nil {
-        t.Fatal(err)
-    }
+	req, err := http.NewRequest("POST", "/api/v1/user", bytes.NewBuffer(jsonData))
+	if err != nil {
+		t.Fatal(err)
+	}
 
-    req.Header.Set("Content-Type", "application/json")
-    w := httptest.NewRecorder()
+	req.Header.Set("Content-Type", "application/json")
+	w := httptest.NewRecorder()
 
-    router.ServeHTTP(w, req)
+	router.ServeHTTP(w, req)
 
-    if w.Code != http.StatusCreated {
-        t.Errorf("Expected status code %d, but got %d", http.StatusCreated, w.Code)
-    }
+	if w.Code != http.StatusCreated {
+		t.Errorf("Expected status code %d, but got %d", http.StatusCreated, w.Code)
+	}
 
-    var createdUser model.User
-    err = json.Unmarshal(w.Body.Bytes(), &createdUser)
-    if err != nil {
-        t.Fatal(err)
-    }
+	var createdUser model.User
+	err = json.Unmarshal(w.Body.Bytes(), &createdUser)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-    if createdUser.LastName != user.LastName || createdUser.Email != user.Email || createdUser.FirstName != user.FirstName{
-        t.Errorf("Expected user %v, but got %v", user, createdUser)
-    }
-
-    }
-}
-
-
-
-func TestCreateTask(t *testing.T){
-    err := godotenv.Load()
-    if err != nil {
-        panic(err)
-    }
-
-    value := os.Getenv("FAUNA_SECRET")
-
-    os.Setenv("FAUNA_SECRET",value)
-
-    router := gin.New()
- 
-    router.POST("api/v1/task",repository.CreateTask)
-    w := httptest.NewRecorder()
-    task := model.Task{
-				Id:"23",
-        Name: "faire les course",
-        Content:"acheter des courgette",
-        State: "IDLE",
-    }
-
-    taskJson,_ := json.Marshal(task)
-    req,_ := http.NewRequest("POST","api/v1/task",bytes.NewBufferString(string(taskJson)))
-    router.ServeHTTP(w,req)
-    t.Error(req)
-    req.Header.Set("Content-Type","application/json")
-
-    if w.Code != http.StatusCreated {
-        t.Errorf("Expected status code %d, but got %d", http.StatusCreated, w.Code)
-    }
-
-    assert.Equal(t,201,w.Code)
-
-    if task.Name == "" || task.Content == "" || task.State == ""{
-        t.Errorf("some information ar empty in task section")
-    }
+	if createdUser.LastName != user.LastName || createdUser.Email != user.Email || createdUser.FirstName != user.FirstName {
+		t.Errorf("Expected user %v, but got %v", user, createdUser)
+	}
 
 }
 
+func TestCreateTask(t *testing.T) {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	value := os.Getenv("FAUNA_SECRET")
+
+	os.Setenv("FAUNA_SECRET", value)
+
+	router := SetUpRouter()
+
+	router.POST("api/v1/task", repository.CreateTask)
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/api/v1/task", strings.NewReader(`{"id":"23","name":"faire les course","content":"acheter des courgette","state":"IDLE"}`))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer ")
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusCreated, w.Code)
+
+}
+
+func TestDeleteTask(t *testing.T) {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	value := os.Getenv("FAUNA_SECRET")
+	os.Setenv("FAUNA_SECRET", value)
+
+	router := SetUpRouter()
+
+	router.DELETE("api/v1/deleteTask", repository.DeleteTask)
+	w := httptest.NewRecorder()
+
+	req, _ := http.NewRequest("DELETE", "api/v1/deleteTask", strings.NewReader(`"id":"23"`))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer")
+	router.ServeHTTP(w, req)
+}
+
+func TestDeleteProject(t *testing.T) {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	value := os.Getenv("FAUNA_SECRET")
+
+	os.Setenv("FAUNA_SECRET", value)
+
+	router := SetUpRouter()
+
+	router.DELETE("api/v1/deleteProjects", repository.DeleteProject)
+
+	w := httptest.NewRecorder()
+
+	req, err := http.NewRequest("DELETE", "api/v1/deleteProjects", strings.NewReader(`{"name":"id"}`))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer")
+	router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+}

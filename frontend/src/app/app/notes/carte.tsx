338f10488b9fbf17b1930aa7821f06156342fa4d
shaw342 <shawanbarua70@gmail.com>
2025-01-01 12:58:16 +0100

    delete frontend folder

--- frontend/src/app/app/notes/carte.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function MyCarte() {
-
-    return (
-            <div className="bg-white  rounded-md shadow-md h-[300px] w-[300px]">    
-            </div>
-    )
-}

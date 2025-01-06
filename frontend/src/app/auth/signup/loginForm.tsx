ea121728d1bbf3dd9833fffbd7934d88f468bf15
shaw342 <shawanbarua70@gmail.com>
2025-01-06 12:28:13 +0100

    update login  form

--- frontend/src/app/auth/signup/loginForm.tsx
+++ frontend/src/app/auth/signup/loginForm.tsx
@@ -13,27 +13,44 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import React, { useState } from "react"
 import axios from "axios"
+import { log } from "console"
+import { getCookie, getCookies, setCookie, deleteCookie, hasCookie } from 'cookies-next/client';
+
+
+
 export const description =
   "A login form with email and password. There's an option to login with Google and a link to sign up if you don't have an account."
+
 export default function LoginForm() {
+  const router = useRouter()
   const [auth, setAuth] = useState({
     email: "",
     password: "",
   })
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target
     setAuth(prevUser => ({
       ...prevUser,
       [name]: value
     }))
-    console.log("State updated:", { ...auth, [name]: value })
+    
   }
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault()
+
     axios.defaults.headers["Content-Type"] = "application/json"
-    axios.post("http://localhost:8080/api/v1/")
-    console.log("Données de formulaire:", auth)
+    axios.post("http://localhost:8080/api/v1/login",auth).then(res =>{
+      setCookie("token",res.data)
+      router.push("/dashboard")
+    }).catch(error =>{
+      console.log('====================================');
+      console.log(error);
+      console.log('====================================');
+    })
   }
+
   return (
     <Card className="mx-auto w-[400px]">
       <CardHeader>
@@ -73,7 +90,7 @@ export default function LoginForm() {
               required
             />
           </div>
-          <Button type="submit" className="w-full">
+          <Button type="submit" className="w-full" onClick={handleSubmit}>
             Login
           </Button>
           <Button variant="outline" className="w-full">
@@ -82,7 +99,7 @@ export default function LoginForm() {
         </form>
         <div className="mt-4 text-center text-sm">
           Don&apos;t have an account?{" "}
-          <Link href="signin" className="underline">
+          <Link href="signin" className="underline" >
             Sign in
           </Link>
         </div>

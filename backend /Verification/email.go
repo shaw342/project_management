668b0a1361ed5cd51a647cac59cf4cc5a11cc462
shaw342 <shawanbarua70@gmail.com>
2025-01-19 12:14:45 +0100

    update email refactor the code

--- backend /Verification/email.go
+++ backend /Verification/email.go
@@ -1,16 +1,15 @@
 package verification
 
 import (
+	"crypto/rand"
 	"fmt"
-	"math/rand"
+	"log"
+	"math/big"
 	"net/smtp"
 	"os"
-	"time"
 
 	"github.com/joho/godotenv"
 )
-
-const letter = "azertyuiopqsdfghjklmwxcvbn"
 
 func SendMail(email string) (string, error) {
 	er := godotenv.Load()
@@ -25,33 +24,45 @@ func SendMail(email string) (string, error) {
 		email,
 	}
 
-	rand.NewSource((time.Now().UnixNano()))
-
 	length := 10
 
-	randomString := generateRandomString(length)
+	randomString, er := GenerateVerificationCode(length)
+
+	if er != nil {
+		log.Fatal(er)
+	}
 
 	smtpHost := "smtp.gmail.com"
 	smtpPort := "587"
 
-	message := []byte("This is your code :" + randomString)
+	subject := "Code de vérification"
+	message := fmt.Sprintf("From: %s\nTo: %s\nSubject: %s\n\nThis is your code: %s", from, email, subject, randomString)
 
 	auth := smtp.PlainAuth("", from, password, smtpHost)
 
-	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, []byte(message))
+
 	if err != nil {
 		fmt.Println(err)
 		return "", err
 	}
 	fmt.Println("Email Sent Successfully!")
 
-	return "Email sent Successfully", nil
+	return randomString, nil
 }
 
-func generateRandomString(length int) string {
-	b := make([]byte, length)
-	for i := range b {
-		b[i] = letter[rand.Intn(len(letter))]
+func GenerateVerificationCode(length int) (string, error) {
+	codeLength := length
+	max := big.NewInt(10) // Les chiffres vont de 0 à 9
+
+	var code string
+	for i := 0; i < codeLength; i++ {
+		num, err := rand.Int(rand.Reader, max)
+		if err != nil {
+			return "", err
+		}
+		code += fmt.Sprintf("%d", num.Int64())
 	}
-	return string(b)
+
+	return code, nil
 }

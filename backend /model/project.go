a125518ad368c7c819d628d9cb666bd5f6bf4e76
shaw342 <shawanbarua70@gmail.com>
2025-01-23 12:38:52 +0100

    update mode project

--- backend /model/project.go
+++ backend /model/project.go
@@ -1,8 +1,12 @@
 package model
 
 type Project struct {
-	Id    string   `json:"id"`
-	Name  string   `json:"name"`
-	Owner string   `json:"owner"`
-	Tasks []string `json:"tasks"`
+	Id          string `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Owner       Owner  `json:"owner"`
+	Tasks       []Task `json:"tasks,omitempty"`
+	Teams       []Team `json:"teams,omitempty"`
+	StartDate   string `json:"startDate"`
+	EndDate     string `json:"endDate"`
 }

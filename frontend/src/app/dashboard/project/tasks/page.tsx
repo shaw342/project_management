1a4c5c56c8e42ba3e11ff1cdd43516dd098f3222
shaw342 <shawanbarua70@gmail.com>
2025-02-13 00:37:37 +0100

    implement tabs in task page

--- frontend/src/app/dashboard/project/tasks/page.tsx
+++ frontend/src/app/dashboard/project/tasks/page.tsx
@@ -1,10 +1,20 @@
 import { TaskManager } from '@/components/task-manager'
+import { Tabs,TabsContent,TabsTrigger,TabsList} from '@/components/ui/tabs'
 
 export default function TaskPage() {
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Gestionnaire de Tâches Détaillées</h1>
+    <main className="container mx-auto p-4 w-[100%]">
+      <Tabs defaultValue="create" className="w-full   mx-auto">
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="create">Create Task</TabsTrigger>
+        <TabsTrigger value="list">Task List</TabsTrigger>
+      </TabsList>
+      <TabsContent value="create" className='flex justify-center'>
       <TaskManager/>
-    </div>
+      </TabsContent>
+      <TabsContent value="list">
+        </TabsContent>
+      </Tabs>
+    </main>
   )
 }

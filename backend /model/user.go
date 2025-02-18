9fca4c6cd769f8c1c0939a5cd717232c58843d69
shaw342 <shawanbarua70@gmail.com>
2025-02-18 20:10:01 +0100

    update model user

--- backend /model/user.go
+++ backend /model/user.go
@@ -1,9 +1,11 @@
 package model
 
 type User struct {
-	Id   string `json:"id"`
-	FirstName string `json:"firstName"`
-	LastName string `json:"lastName"`
-	Email string `json:"email"`
-	Password string `json:"password"`
+	Id          string `json:"id" fauna:"id"`
+	Email       string `json:"email" fauna:"email"`
+	FirstName   string `json:"firstName" fauna:"firstName"`
+	LastName    string `json:"lastName" fauna:"lastName"`
+	Password    string `json:"password" fauna:"password"`
+	AccessLevel string `json:"accessLevel" fauna:"accessLevel"`
+	Status      string `json:"status" fauna:"status"`
 }

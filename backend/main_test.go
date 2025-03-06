7718fb0e6a79bf0b108ba3860f8cb41d1fb71616
shaw342 <shawanbarua70@gmail.com>
2025-03-06 11:58:02 +0100

    update test that call setupRouter

--- backend/main_test.go
+++ backend/main_test.go
@@ -3,32 +3,38 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 
-	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
+	"github.com/joho/godotenv"
 	"github.com/shaw342/projet_argile/backend/model"
+	"github.com/shaw342/projet_argile/backend/routes"
 	"github.com/stretchr/testify/assert"
 )
 
 
-func SetupRouter() *gin.Engine{
-	r := gin.Default()
-	return r
-}
 
-func RegisterTest(t *testing.T){
-	router := SetupRouter()
+func TestRegister(t *testing.T){
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
+	value := os.Getenv("FAUNA_SECRET")
+	os.Setenv("FAUNA_SECRET", value)
+	router := routes.SetupRouter()
 
 	user := model.User{
-
-		UserId: uuid.New().String(),
 		FirstName: "shawan",
 		LastName: "yousull",
+		Password: "123456",
 		Email: "local@gmail.com",
 	}
+
+	fmt.Print(user)
 
 	jsonData,err := json.Marshal(&user)
 
@@ -36,16 +42,17 @@ func RegisterTest(t *testing.T){
 		t.Fatalf("error to encoding in json format")
 	}	
 
-	req,err := http.NewRequest("POST","http://localhost:8080/api/v1/register",bytes.NewBuffer(jsonData))
+	req,err := http.NewRequest("POST","http://localhost/api/v1/register",bytes.NewBuffer(jsonData))
 
 	if err != nil{
 		t.Fatalf("error send request json data")
 	}
 
 	req.Header.Set("Content-Type","application/json")
-
 	w := httptest.NewRecorder()
 	router.ServeHTTP(w,req)
 
-	assert.Equal(t,w.Code,http.StatusCreated)
-} 
+	assert.Equal(t,http.StatusCreated,w.Code)
+}
+
+

83b24bf75973cfb1b8cb84ad08fb5682a09a775a
shaw342 <shawanbarua70@gmail.com>
2025-03-23 17:05:49 +0100

    create migration up for users

---
+++ backend/migrations/v1_create_users_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users(
+    user_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    firstName VARCHAR(100) NOT NULL,
+    lastName VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password TEXT NOT NULL,
+    createAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)

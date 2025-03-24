a787c9481d4354b45504943b6a845ddfe2edc5ac
shaw342 <shawanbarua70@gmail.com>
2025-03-24 05:07:38 +0100

    add test : create task

--- backend/main_test.go
+++ backend/main_test.go
@@ -3,56 +3,72 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"net/http/httptest"
-	"os"
 	"testing"
 
-	"github.com/joho/godotenv"
 	"github.com/shaw342/projet_argile/backend/model"
 	"github.com/shaw342/projet_argile/backend/routes"
 	"github.com/stretchr/testify/assert"
 )
 
-
-
-func TestRegister(t *testing.T){
-	err := godotenv.Load()
-	if err != nil {
-		fmt.Println("Error loading .env file")
-	}
-
-	value := os.Getenv("FAUNA_SECRET")
-	os.Setenv("FAUNA_SECRET", value)
+func TestRegister(t *testing.T) {
 	router := routes.SetupRouter()
 
 	user := model.User{
 		FirstName: "shawan",
-		LastName: "yousull",
-		Password: "123456",
-		Email: "local@gmail.com",
+		LastName:  "yousull",
+		Password:  "123456",
+		Email:     "local@gmail.com",
 	}
 
-	fmt.Print(user)
+	jsonData, err := json.Marshal(&user)
 
-	jsonData,err := json.Marshal(&user)
-
-	if err != nil{
+	if err != nil {
 		t.Fatalf("error to encoding in json format")
-	}	
+	}
 
-	req,err := http.NewRequest("POST","http://localhost/api/v1/register",bytes.NewBuffer(jsonData))
+	req, err := http.NewRequest("POST", "http://localhost/api/v1/register", bytes.NewBuffer(jsonData))
 
-	if err != nil{
+	if err != nil {
 		t.Fatalf("error send request json data")
 	}
 
-	req.Header.Set("Content-Type","application/json")
+	req.Header.Set("Content-Type", "application/json")
 	w := httptest.NewRecorder()
-	router.ServeHTTP(w,req)
+	router.ServeHTTP(w, req)
 
-	assert.Equal(t,http.StatusCreated,w.Code)
+	assert.Equal(t, http.StatusCreated, w.Code)
 }
 
+func TestCreateTask(t *testing.T) {
+	router := routes.SetupRouter()
 
+	task := model.Task{
+		TaskId:  "1",
+		Name:    "implement authentication",
+		Content: "decription",
+		Assign:  "124",
+	}
+
+	jsonData, err := json.Marshal(&task)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	req, err := http.NewRequest("POST", "http://localhost:8080/api/task/create", bytes.NewBuffer(jsonData))
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	w := httptest.NewRecorder()
+
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusCreated, w.Code)
+
+}

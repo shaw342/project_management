5ad54c7ba6f7beda3aa949c1703b992d0e0b78c3
shaw342 <shawanbarua70@gmail.com>
2025-03-27 09:13:29 +0100

    update: CreateUser function change type userId and add user.CreateAt

--- backend/service/user_service.go
+++ backend/service/user_service.go
@@ -3,6 +3,7 @@ package service
 import (
 	"database/sql"
 
+	_ "github.com/lib/pq"
 	"github.com/shaw342/projet_argile/backend/model"
 )
 
@@ -16,10 +17,10 @@ func NewUserService(db *sql.DB) *UserService {
 
 func (s *UserService) CreateUser(user model.User) (model.User, error) {
 
-	var userId int
+	var userId []uint8
 
-	err := s.db.QueryRow(`INSERT INTO users(firstName,lastName,email,password,status)
-		VALUES ($1,$2,$3,$4,$5,$6) RETURNING id`, user.FirstName, user.LastName, user.Email, user.Password, user.Status).Scan(&userId)
+	err := s.db.QueryRow(`INSERT INTO users(firstName,lastName,email,password,status,createat)
+		VALUES ($1,$2,$3,$4,$5,$6) RETURNING user_id`, user.FirstName, user.LastName, user.Email, user.Password, user.Status, user.CreateAt).Scan(&userId)
 
 	if err != nil {
 		return model.User{}, err

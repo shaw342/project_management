3b2cb6b2c7a9cd29059c056c15174870d3c74c7a
shaw342 <shawanbarua70@gmail.com>
2025-03-27 02:21:31 +0100

    update config for test conexion in database

--- backend/config/dbConfig.go
+++ backend/config/dbConfig.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"os"
 
+	_ "github.com/lib/pq"
+
 	"github.com/joho/godotenv"
 )
 
@@ -20,15 +22,20 @@ func ConnectDB() *sql.DB {
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	port := os.Getenv("DB_PORT")
-	db_name := os.Getenv("DB_NAME")
+	dbName := os.Getenv("DB_NAME")
 
-	constr := fmt.Sprintf(`host=%s post=%s user=%s dbname=%s password=%s sslmode=disable`, host, user, password, port, db_name)
-
+	constr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", user, password, host, port, dbName)
 	db, err := sql.Open("postgres", constr)
 
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	if err := db.Ping(); err != nil {
+		log.Fatal("error to connect in database")
+	}
+
+	fmt.Println("connect to postgres")
 
 	return db
 }

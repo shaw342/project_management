d0a31d32ac328a42f1f75021ba09b0fb6fb3d76a
shaw342 <shawanbarua70@gmail.com>
2025-03-28 08:21:58 +0100

    refactor the code for main

--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
-
-	"github.com/golang-migrate/migrate"
-	"github.com/joho/godotenv"
 	"github.com/shaw342/projet_argile/backend/routes"
 )
 
@@ -14,33 +8,5 @@ func main() {
 
 	router := routes.SetupRouter()
 
-	runMigration()
-
-	router.Run()
-}
-
-func runMigration() {
-	err := godotenv.Load()
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	user := os.Getenv("DB_USER")
-	host := os.Getenv("DB_HOST")
-	password := os.Getenv("DB_PASSWORD")
-	port := os.Getenv("DB_PORT")
-	dbName := os.Getenv("DB_NAME")
-
-	constr := fmt.Sprintf("posgres://%s:%s@%s:%s/%s?sslmode=disable", user, password, host, port, dbName)
-
-	m, err := migrate.New("./migration", constr)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
-		log.Fatal(err)
-	}
+	router.Run(":8080")
 }

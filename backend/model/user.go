7edc65723406787c928ef7c8f7c179ac8a301c71
shaw342 <shawanbarua70@gmail.com>
2025-03-28 02:11:50 +0100

    update: change userId type

--- backend/model/user.go
+++ backend/model/user.go
@@ -1,11 +1,11 @@
 package model
 
 type User struct {
-	UserId      string `json:"UserId"`
-	Email       string `json:"email"`
-	FirstName   string `json:"firstName"`
-	LastName    string `json:"lastName"`
-	Password    string `json:"password"`
-	AccessLevel string `json:"accessLevel"`
-	Status      string `json:"status"`
+	UserId    []uint8 `json:"user_id"`
+	Email     string  `json:"email"`
+	FirstName string  `json:"firstName"`
+	LastName  string  `json:"lastName"`
+	Password  string  `json:"password"`
+	Status    string  `json:"status"`
+	CreateAt  string  `json:"createat"`
 }

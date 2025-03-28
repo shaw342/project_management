dcb886930fe67b2b11efff3528d5750050ed469f
shaw342 <shawanbarua70@gmail.com>
2025-03-28 07:44:50 +0100

    add test for email change url for test register

--- backend/main_test.go
+++ backend/main_test.go
@@ -6,20 +6,27 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
+	verification "github.com/shaw342/projet_argile/backend/Verification"
+	"github.com/shaw342/projet_argile/backend/config"
 	"github.com/shaw342/projet_argile/backend/model"
 	"github.com/shaw342/projet_argile/backend/routes"
+	"github.com/shaw342/projet_argile/backend/service"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestRegister(t *testing.T) {
 	router := routes.SetupRouter()
+	createat := time.DateOnly
 
 	user := model.User{
 		FirstName: "shawan",
 		LastName:  "yousull",
 		Password:  "123456",
 		Email:     "local@gmail.com",
+		Status:    "active",
+		CreateAt:  createat,
 	}
 
 	jsonData, err := json.Marshal(&user)
@@ -28,7 +35,7 @@ func TestRegister(t *testing.T) {
 		t.Fatalf("error to encoding in json format")
 	}
 
-	req, err := http.NewRequest("POST", "http://localhost/api/v1/register", bytes.NewBuffer(jsonData))
+	req, err := http.NewRequest("POST", "http://localhost:8080/api/v1/register", bytes.NewBuffer(jsonData))
 
 	if err != nil {
 		t.Fatalf("error send request json data")
@@ -71,4 +78,31 @@ func TestCreateTask(t *testing.T) {
 
 	assert.Equal(t, http.StatusCreated, w.Code)
 
+}
+
+func TestCheckUser(t *testing.T) {
+	db := config.ConnectDB()
+	userService := service.NewUserService(db)
+
+	email := "local@gmail.com"
+
+	response, err := userService.CheckUser(email)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	assert.Equal(t, true, response)
+}
+
+func TestEmail(t *testing.T) {
+
+	email := "localfv9efnv"
+	result, err := verification.SendEmail(email)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	assert.Equal(t, "localfv9efnv", result)
 }

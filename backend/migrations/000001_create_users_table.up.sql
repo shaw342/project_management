81df82555ef48afe339d073ba10642ff0bdbe61e
shaw342 <shawanbarua70@gmail.com>
2025-03-28 08:37:06 +0100

    update: up users table

--- backend/migrations/000001_create_users_table.up.sql
+++ backend/migrations/000001_create_users_table.up.sql
@@ -2,8 +2,8 @@ CREATE TABLE users(
     user_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     firstName VARCHAR(100) NOT NULL,
     lastName VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
     password TEXT NOT NULL,
     status VARCHAR(100) NOT NULL,
-    createAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    createat TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );

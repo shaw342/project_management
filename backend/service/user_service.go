dd276caaadeda2b12bb27ff9797b453d73f39e20
shaw342 <shawanbarua70@gmail.com>
2025-03-28 02:04:27 +0100

    implement CheckUser:verifu user Exists

--- backend/service/user_service.go
+++ backend/service/user_service.go
@@ -19,12 +19,27 @@ func (s *UserService) CreateUser(user model.User) (model.User, error) {
 
 	var userId []uint8
 
-	err := s.db.QueryRow(`INSERT INTO users(firstName,lastName,email,password,status,createat)
+	QueryErr := s.db.QueryRow(`INSERT INTO users(firstName,lastName,email,password,status,createat)
 		VALUES ($1,$2,$3,$4,$5,$6) RETURNING user_id`, user.FirstName, user.LastName, user.Email, user.Password, user.Status, user.CreateAt).Scan(&userId)
 
-	if err != nil {
-		return model.User{}, err
+	if QueryErr != nil {
+		return model.User{}, QueryErr
 	}
 
 	return user, nil
+}
+
+func (s *UserService) CheckUser(email string) (bool, error) {
+
+	var result bool
+
+	queryString := "SELECT EXISTS(SELECT 1 FROM users WHERE firstname = $1)"
+
+	err := s.db.QueryRow(queryString, email).Scan(&result)
+
+	if err != nil {
+		return false, err
+	}
+
+	return result, nil
 }

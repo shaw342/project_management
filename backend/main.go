feb01e5ce25c2f87650958e866466668536fac55
shaw342 <shawanbarua70@gmail.com>
2025-03-29 14:45:07 +0100

    update: use goroutine and call connect db in main

--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,33 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/shaw342/projet_argile/backend/config"
 	"github.com/shaw342/projet_argile/backend/routes"
 )
 
 func main() {
+	db := config.ConnectDB()
+	defer db.Close()
 
-	router := routes.SetupRouter()
+	router := routes.SetupRouter(db)
 
 	router.Run(":8080")
+
+	go func() {
+		if err := router.Run(":8080"); err != nil && err != http.ErrServerClosed {
+			log.Fatal("Could not run serveur")
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server")
+
 }

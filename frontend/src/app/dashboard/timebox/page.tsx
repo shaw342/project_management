703360cf95d27d5998640b61df4c3fae6c59993b
shaw342 <shawanbarua70@gmail.com>
2025-04-08 22:23:46 +0200

    create time box section

---
+++ frontend/src/app/dashboard/timebox/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import { TimeBox } from "@/components/time-box";
+
+export default function page() {
+    
+    return (
+        <div>
+           <TimeBox/> 
+        </div>
+    )
+}

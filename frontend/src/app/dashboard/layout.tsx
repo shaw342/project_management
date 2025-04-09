202ef143687aad6f864047dfb0b70b7820fe775a
shaw342 <shawanbarua70@gmail.com>
2025-04-09 12:46:32 +0200

    call user-profile-dropdown component

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/app-sidebar"
+import UserProfileDropdown  from "@/components/user-profile-dropdown";
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -9,6 +10,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <SidebarTrigger />
         {children}
       </main>
+      <UserProfileDropdown/>
     </SidebarProvider>
   )
 }

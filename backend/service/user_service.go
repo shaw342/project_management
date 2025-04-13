bf384f3d2966ad9170c4fb61f806446849047ef1
shaw342 <shawanbarua70@gmail.com>
2025-04-13 21:12:58 +0200

    create getAuth and call HashPassword function in register

--- backend/service/user_service.go
+++ backend/service/user_service.go
@@ -2,13 +2,21 @@ package service
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/lib/pq"
 	"github.com/shaw342/projet_argile/backend/model"
+	utils_s "github.com/shaw342/projet_argile/backend/utils"
 )
 
 type UserService struct {
 	db *sql.DB
+}
+
+type ServiceInterface struct {
+	CreateUser (model.User)
+	CheckUser  (model.User)
+	GetUser    (model.User)
 }
 
 func NewUserService(db *sql.DB) *UserService {
@@ -19,8 +27,14 @@ func (s *UserService) CreateUser(user model.User) (model.User, error) {
 
 	var userId []uint8
 
+	hashPassword, err := utils_s.HashPassword(user.Password)
+
+	if err != nil {
+		return model.User{}, err
+	}
+
 	QueryErr := s.db.QueryRow(`INSERT INTO users(firstName,lastName,email,password,status,createat)
-		VALUES ($1,$2,$3,$4,$5,$6) RETURNING user_id`, user.FirstName, user.LastName, user.Email, user.Password, user.Status, user.CreateAt).Scan(&userId)
+		VALUES ($1,$2,$3,$4,$5,$6) RETURNING user_id`, user.FirstName, user.LastName, user.Email, hashPassword, user.Status, user.CreateAt).Scan(&userId)
 
 	if QueryErr != nil {
 		return model.User{}, QueryErr
@@ -44,12 +58,50 @@ func (s *UserService) CheckUser(email string) (bool, error) {
 	return result, nil
 }
 
+func (s *UserService) GetAllUser() ([]model.User, error) {
+	var result []model.User
+
+	queryString := "SELECT * FROM users"
+
+	row, err := s.db.Query(queryString)
+
+	if err != nil {
+		return []model.User{}, err
+	}
+
+	for row.Next() {
+		var user model.User
+		if err := row.Scan(&user.UserId, &user.Email, &user.FirstName, &user.LastName); err != nil {
+			return []model.User{}, err
+		}
+		result = append(result, user)
+	}
+
+	return result, nil
+}
+
+func (s *UserService) GetAuth(email string) (string, error) {
+	var auth model.Auth
+
+	queryString := "SELECT password FROM users WHERE email = $1"
+
+	err := s.db.QueryRow(queryString, email).Scan(&auth.Password)
+
+	if err != nil {
+		return "", err
+	}
+
+	return auth.Password, nil
+}
+
 func (s *UserService) GetUser(email string) (model.User, error) {
 	var user model.User
 
 	queryString := "SELECT * FROM users WHERE email = $1"
 
-	err := s.db.QueryRow(queryString, email).Scan(&user)
+	err := s.db.QueryRow(queryString, email).Scan(&user.FirstName, &user.LastName, &user.CreateAt, &user.Password, user.UserId, &user.Status, user.Email)
+
+	fmt.Println(user.Password)
 
 	if err != nil {
 

d08928e4dd7a20b683389b51bf02d9d2c5855711
shaw342 <shawanbarua70@gmail.com>
2025-04-13 21:18:51 +0200

    create login

--- backend/controllers/user_controllers.go
+++ backend/controllers/user_controllers.go
@@ -1,17 +1,46 @@
 package controllers
 
 import (
-	"fmt"
+	"crypto/ecdsa"
+	"crypto/x509"
+	"encoding/pem"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/joho/godotenv"
 	"github.com/shaw342/projet_argile/backend/model"
 	"github.com/shaw342/projet_argile/backend/service"
+	"golang.org/x/crypto/bcrypt"
 )
+
+var ecdsaPrivateKey *ecdsa.PrivateKey
 
 type UserController struct {
 	userService *service.UserService
+}
+
+func init() {
+	keyData, err := os.ReadFile("ecdsa_private_key.pem")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	block, _ := pem.Decode(keyData)
+
+	if block == nil || block.Type == "PRIVATE KEY" {
+		log.Fatal(err)
+	}
+
+	ecdsaPrivateKey, err = x509.ParseECPrivateKey(block.Bytes)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func NewUserController(userService *service.UserService) *UserController {
@@ -32,9 +61,7 @@ func (c *UserController) Register(ctx *gin.Context) {
 		log.Fatal(err)
 	}
 
-	fmt.Println(check)
-
-	if check == true {
+	if check {
 		ctx.JSON(http.StatusAccepted, gin.H{"server": "user already exist"})
 		return
 	}
@@ -46,7 +73,6 @@ func (c *UserController) Register(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusCreated, result)
-
 }
 
 func (c *UserController) Welcome(ctx *gin.Context) {
@@ -76,6 +102,45 @@ func (c *UserController) CheckUser(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusAccepted, result)
+}
+
+func (c *UserController) Login(ctx *gin.Context) {
+	auth := model.Auth{}
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := ctx.ShouldBindJSON(&auth); err != nil {
+		log.Fatal(err)
+	}
+	password, err := c.userService.GetAuth(auth.Email)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	existErro := bcrypt.CompareHashAndPassword([]byte(password), []byte(auth.Password))
+
+	if existErro != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"eror": "wrong password"})
+	}
+
+	claims := &jwt.MapClaims{
+		"exp":    jwt.NewNumericDate(time.Now().Add(time.Hour * 1)).Unix(),
+		"issuer": "test",
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+
+	tokenString, err := token.SignedString(ecdsaPrivateKey)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx.JSON(http.StatusAccepted, gin.H{"success": tokenString})
 }
 
 func (c *UserController) GetUser(ctx *gin.Context) {

ff51b533bc2071a2a8196cc0d86223f42b6600a7
shaw342 <shawanbarua70@gmail.com>
2025-04-15 00:52:27 +0200

    update:add members field

--- backend/model/team.go
+++ backend/model/team.go
@@ -1,7 +1,8 @@
 package model
 
 type Team struct {
-	Id    string `json:"Id"`
-	Name  string `json:"name"`
-	Owner string `json:"ownerId"`
+	Id      string `json:"Id"`
+	Name    string `json:"name"`
+	Owner   string `json:"owner_id"`
+	Members []User `json:"members"`
 }

0361a036a733be185089b95b09626db53e0734b0
shaw342 <shawanbarua70@gmail.com>
2025-04-17 11:46:41 +0200

    create manager controllers

---
+++ backend/controllers/manager_controllers.go
@@ -0,0 +1,33 @@
+package controllers
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/shaw342/projet_argile/backend/model"
+	"github.com/shaw342/projet_argile/backend/service"
+)
+
+type ManagerController struct {
+	service *service.ManagerService
+}
+
+func NewMangerController(service *service.ManagerService) *ManagerController {
+	return &ManagerController{service: service}
+}
+
+func (c *ManagerController) CreateTask(ctx *gin.Context) {
+	task := model.Task{}
+
+	if err := ctx.ShouldBindJSON(&task); err != nil {
+		log.Fatal(err)
+	}
+	result, err := c.service.CreateTask(task)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{"successFull": result})
+}

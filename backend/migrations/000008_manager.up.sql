976e214febed8bf8f2a37474b09efb313e062832
shaw342 <shawanbarua70@gmail.com>
2025-04-19 17:33:59 +0200

    create owner table

---
+++ backend/migrations/000008_manager.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS manager(
+    manager_id UUID PRIMARY KEY NOT NULL,
+    user_id UUID NOT NULL,
+    owner_id UUID NOT NULL,
+    CONSTRAINT fk_owner
+    FOREIGN KEY (owner_id) REFERENCES owner(owner_id),
+    CONSTRAINT fk_user
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);

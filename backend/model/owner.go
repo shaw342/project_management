4942921f57195d4605620de0104c8badbfacf4d9
shaw342 <shawanbarua70@gmail.com>
2025-04-21 11:03:16 +0200

    update owner model

--- backend/model/owner.go
+++ backend/model/owner.go
@@ -1,10 +1,6 @@
 package model
 
 type Owner struct {
-	Id        string    `json:"id" fauna:"id"`
-	FirstName string    `json:"firstName" fauna:"firstName"`
-	LastName  string    `json:"lastName" fauna:"lastName"`
-	Email     string    `json:"email" fauna:"email"`
-	Team      []Manager `json:"team" fauna:"team"`
-	Project   []Project `json:"project" fauna:"project"`
+	OwnerId string `json:"owner_id"`
+	UserId  string `json:"user_id"`
 }

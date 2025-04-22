3d91d6d96f3596dea91219efea1a5fe54ace55b0
shaw342 <shawanbarua70@gmail.com>
2025-04-22 20:18:43 +0200

    add Level type

--- backend/model/state.go
+++ backend/model/state.go
@@ -8,3 +8,12 @@ const (
 	Completed  State = "Completed"
 	OnHold     State = "On Hold"
 )
+
+type Level string
+
+const (
+	High     Level = "High"
+	Medium   Level = "Medium"
+	Low      Level = "Level"
+	Undefind Level = "Undefind"
+)

bbef7cc24c243bec8e9412056857d9e5559a4ceb
shaw342 <shawanbarua70@gmail.com>
2025-04-22 20:23:08 +0200

    model/project.go

--- backend/model/project.go
+++ backend/model/project.go
@@ -1,12 +1,16 @@
 package model
 
+import (
+	"time"
+)
+
 type Project struct {
-	ProjectId   string `json:"projectId"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Owner       Owner  `json:"owner"`
-	Tasks       []Task `json:"tasks,omitempty"`
-	Teams       []Team `json:"teams"`
-	StartDate   string `json:"startDate"`
-	EndDate     string `json:"endDate"`
+	ProjectId   string    `json:"project_id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Status      State     `json:"status"`
+	OwnerId     string    `json:"owner_id"`
+	ManagerId   string    `json:"manager_id"`
+	StartDate   time.Time `json:"startDate"`
+	EndDate     time.Time `json:"endDate"`
 }

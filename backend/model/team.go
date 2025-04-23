424afba0e9e3a42133e4b3c8957ae138a56ed05c
shaw342 <shawanbarua70@gmail.com>
2025-04-23 17:13:48 +0200

    delete members field

--- backend/model/team.go
+++ backend/model/team.go
@@ -3,6 +3,5 @@ package model
 type Team struct {
 	Id      string `json:"Id"`
 	Name    string `json:"name"`
-	Owner   string `json:"owner_id"`
-	Members []User `json:"members"`
+	OwnerId string `json:"owner_id"`
 }

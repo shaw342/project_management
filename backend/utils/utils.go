82b9c14ac238ffb6ac4f0d3befc2d9550433e702
shaw342 <shawanbarua70@gmail.com>
2025-05-01 12:17:23 +0200

    create hashpassword function

---
+++ backend/utils/utils.go
@@ -0,0 +1,10 @@
+package utils_s
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}

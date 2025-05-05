1adfe149974c060bc4fa06bbb7bd8df16c88f024
shaw342 <shawanbarua70@gmail.com>
2025-05-05 08:46:09 +0200

    update migration

--- backend/migration/000006_project.up.sql
+++ backend/migration/000006_project.up.sql
@@ -1,10 +1,12 @@
 CREATE TABLE IF NOT EXISTS project (
+    id INT NOT NULL,
     project_id UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
     name VARCHAR(100) NOT NULL,
     description VARCHAR(100) NOT NULL,
     owner_id UUID NOT NULL,
+    startDate DATE NOT NULL,
+    endDate DATE NOT NULL,
     CONSTRAINT fk_owner
     FOREIGN KEY (owner_id) REFERENCES owner(owner_id),
-    teams VARCHAR(100),
-    startDate DATE NOT NULL
+    teams VARCHAR(100)
 );

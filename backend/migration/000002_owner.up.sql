6ecb653ea4fe665bc95534bbe518ba9e25ec2004
ShawanBarua <124527999+shaw342@users.noreply.github.com>
2025-05-08 16:40:38 +0200

    update owner table and manager

--- backend/migration/000002_owner.up.sql
+++ backend/migration/000002_owner.up.sql
@@ -1,6 +1,9 @@
 CREATE TABLE IF NOT EXISTS owner(
     owner_id UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
     user_id UUID NOT NULL,
+    manager_id UUID NOT NULL,
+    CONSTRAINT fk_manager
+    FOREIGN KEY (manager_id) REFERENCES manager(manager_id),
     CONSTRAINT fk_user
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );

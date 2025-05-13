5fb56307e810b20ea67cde74d0c0df77e1c03e76
ShawanBarua <124527999+shaw342@users.noreply.github.com>
2025-05-13 19:32:14 +0200

    implement inbox

---
+++ frontend/src/app/dashboard/inbox/page.tsx
@@ -0,0 +1,10 @@
+import { InvitationsList } from "@/components/invitations-list"
+
+
+export default function InboxPage() {
+  return (
+    <div className="container py-8 max-w-5xl">
+      <InvitationsList />
+    </div>
+  )
+}

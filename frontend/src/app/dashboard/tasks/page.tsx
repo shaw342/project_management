97a36d2dd751913a3b1c5d178f4ff07ccc6e2143
ShawanBarua <124527999+shaw342@users.noreply.github.com>
2025-08-03 20:59:04 +0200

    update call taskManager component

--- frontend/src/app/dashboard/tasks/page.tsx
+++ frontend/src/app/dashboard/tasks/page.tsx
@@ -1,23 +1,11 @@
 "use client"
-import { CreateTask } from '@/components/createTask'
-import { Tabs,TabsContent,TabsList,TabsTrigger } from "@/components/ui/tabs";
+import { TaskManager } from "@/components/task-manager";
 
 
 export default function TaskPage() {
   return (
-    <div className="container mx-auto p-4">
-      <Tabs>
-        <TabsList>
-          <TabsTrigger value="all">All tasks</TabsTrigger>
-          <TabsTrigger value="create">Create task</TabsTrigger>
-        </TabsList>
-        <TabsContent value='all'>
-          
-        </TabsContent>
-        <TabsContent value="create">
-        <CreateTask />
-        </TabsContent>
-      </Tabs>
+    <div>
+      <TaskManager/>
     </div>
   )
 }

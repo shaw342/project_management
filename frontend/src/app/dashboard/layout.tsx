4ad3379f7acb8f82fd63b2663c829dd29827f78a
ShawanBarua <124527999+shaw342@users.noreply.github.com>
2025-08-03 21:00:10 +0200

    update: implement notification bell

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -1,16 +1,20 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/app-sidebar"
 import UserProfileDropdown  from "@/components/user-profile-dropdown";
+import { NotificationBell } from "@/components/notification-bell";
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main>
+      <main className="w-[90%]">
         <SidebarTrigger />
         {children}
       </main>
-      <UserProfileDropdown/>
+      <div className="h-[150px] flex justify-evenly items-center flex-col">
+         <UserProfileDropdown/> 
+         <NotificationBell/>
+      </div>
     </SidebarProvider>
   )
 }
